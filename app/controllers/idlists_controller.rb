391547e3b62bb8c8f363c5c329dce72dfd6b9ab6
Curtis <curtis816@gmail.com>
2015-10-30 00:57:45 +0800

    加入conver_code_to_country 可把首字母換成該縣市

--- app/controllers/idlists_controller.rb
+++ app/controllers/idlists_controller.rb
@@ -1,12 +1,13 @@
 class IdlistsController < ApplicationController
   def index
-
   end
 
   def id_check
+    @t1 = Time.now
     @id = params[:id]
 
     @check = vaild_id?(@id)
+    @country = convert_code_to_country(@id)
 
     render 'index'
   end
@@ -14,7 +15,7 @@ class IdlistsController < ApplicationController
   private
 
   def vaild_id?(id)
-    $temp = 0
+    $check_number = 0
     $id_chars = id.chars
     $cal = [1,9,8,7,6,5,4,3,2,1,1]
     map = { 'A' => [1, 0], 'B' => [1, 1], 'C' => [1, 2], 'D' => [1, 3],
@@ -27,14 +28,22 @@ class IdlistsController < ApplicationController
     numbers = map[$id_chars.shift]+$id_chars.map(&:to_i)
 
     while $i < 11 do
-      $temp += numbers[$i] * $cal[$i]
+      $check_number += numbers[$i] * $cal[$i]
       $i += 1
     end
-    $check_number = $temp
-    if $check_number % 10 == 0
-      return true
-    else
-      return false
-    end
+
+    $check_number % 10 == 0
+
+  end
+
+  def convert_code_to_country(id)
+    map = { 'A' => "台北市", 'B' => "台中市", 'C' => "基隆市", 'D' => "台南市",
+     'E' => "高雄市", 'F' => "新北市", 'G' => "宜蘭縣", 'H' => "桃園市", 'I' => "嘉義市",
+      'J' => "嘉義市", 'K' => "苗栗縣", 'L' => "台中縣", 'M' => "南投縣", 'N' => "彰化縣",
+      'O' => "新竹市", 'P' => "雲林縣", 'Q' => "嘉義縣", 'R' => "台南縣",
+      'S' => "高雄縣", 'T' => "屏東縣", 'U' => "花蓮縣", 'V' => "台東縣", 'W' => "金門縣",
+      'X' => "澎湖縣", 'Y' => "陽明山管理局", 'Z' => "連江縣"}
+
+    return map[id[0]]
   end
 end
